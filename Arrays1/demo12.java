7dac22fcd755083a3251f04b7426d9b3697fce0b
Sahil <sahil.brahmbhatt710@gmail.com>
2025-12-27 19:34:33 +0530

    Added the Arrays1 folder

---
+++ Arrays1/demo12.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class demo12 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = input.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = input.nextInt();
+
+        int[][] a = new int[rows][cols];
+        int[][] b = new int[rows][cols];
+        int[][] sum = new int[rows][cols];
+
+       
+        System.out.println("Enter elements of first matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                a[i][j] = input.nextInt();
+            }
+        }
+
+        
+        System.out.println("Enter elements of second matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                b[i][j] = input.nextInt();
+            }
+        }
+
+     
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        System.out.println("Sum of two matrices:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
