b78665be003266f8dced920466e831ecd8be5963
Sahil <sahil.brahmbhatt710@gmail.com>
2025-12-30 19:24:21 +0530

    Added the Arrays1 folder

---
+++ Arrays1/demo15.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class demo15 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = input.nextInt();
+
+        if (n < 2) {
+            System.out.println("Need at least 2 elements.");
+            return;
+        }
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } 
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("Second largest element does not exist.");
+        } else {
+            System.out.println("Second largest element = " + secondLargest);
+        }
+    }
+}
